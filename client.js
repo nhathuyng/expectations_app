eeb1f13a400353c2e772b19218533eaaf389eae8
Huy Nguyen <huy.nguyen@codestra.io>
2022-12-28 17:21:19 +0100

    changed yml

---
+++ client.js
@@ -0,0 +1,36 @@
+const { Client } = require('pg');
+
+const pgclient = new Client({
+	host: process.env.POSTGRES_HOST,
+	port: process.env.POSTGRES_PORT,
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
+});
+
+pgclient.connect();
+
+const schema = 'CREATE SCHEMA app';
+
+const table =
+	'CREATE TABLE app.customer (id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))';
+const text = 'INSERT INTO app.customer(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *';
+const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com'];
+
+pgclient.query(schema, (err, res) => {
+	if (err) throw err;
+});
+
+pgclient.query(table, (err, res) => {
+	if (err) throw err;
+});
+
+pgclient.query(text, values, (err, res) => {
+	if (err) throw err;
+});
+
+pgclient.query('SELECT * FROM app.customer', (err, res) => {
+	if (err) throw err;
+	console.log(err, res.rows); // Print the data in student table
+	pgclient.end();
+});
