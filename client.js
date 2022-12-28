77bfbd51fb48ca591db8ea73a1e887f5e2f515c4
Huy Nguyen <huy.nguyen@codestra.io>
2022-12-28 19:13:17 +0100

    added pg dependency

--- client.js
+++ client.js
@@ -3,9 +3,9 @@ const { Client } = require('pg');
 const pgclient = new Client({
 	host: process.env.POSTGRES_HOST,
 	port: process.env.POSTGRES_PORT,
-	user: process.env.POSTGRES_USER,
-	password: process.env.POSTGRES_PASSWORD,
-	database: process.env.POSTGRES_DB,
+	user: 'huy',
+	password: 'password',
+	database: 'data_quality',
 });
 
 pgclient.connect();
